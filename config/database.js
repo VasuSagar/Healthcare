fc56430f2c2de7dc6d8c918171fbdfac4658315e
VasuSagar <61358629+VasuSagar@users.noreply.github.com>
2020-06-24 14:17:44 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports={
-    database:'mongodb+srv://Vasu:ugmZNFS82@nodeserver.bkmdu.mongodb.net/nodeserver?retryWrites=true&w=majority',
+    //database:'mongodb+srv://Vasu:ugmZNFS82@nodeserver.bkmdu.mongodb.net/nodeserver?retryWrites=true&w=majority',
+    database:'mongodb://localhost:27017/AngAuth',
     secret:'yoursecret'
 }
